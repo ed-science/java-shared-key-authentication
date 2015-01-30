a31f4a24fed8604618e962981b1ace557d76be1f
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-01-30 15:57:47 -0200

    Repositorio de usuarios

---
+++ src/br/com/rcaneppele/rest/repository/Usuarios.java
@@ -0,0 +1,28 @@
+package br.com.rcaneppele.rest.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.rcaneppele.rest.resource.Usuario;
+
+public class Usuarios {
+
+	private List<Usuario> cadastrados = new ArrayList<>();
+	
+	public Usuarios() {
+		cadastrados.add(new Usuario("fulano@fulano.com", "Fulano"));
+		cadastrados.add(new Usuario("beltrano@beltrano.com", "Beltrano"));
+		cadastrados.add(new Usuario("ciclano@ciclano.com", "Ciclano"));
+	}
+	
+	public boolean isCadastrado(String login) {
+		for (Usuario cadastrado : cadastrados) {
+			if (cadastrado.getLogin().equals(login)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+}
