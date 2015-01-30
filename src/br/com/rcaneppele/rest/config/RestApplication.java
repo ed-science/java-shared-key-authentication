df8e1276f4da336c74041363ce430990b1b293f9
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-01-30 09:45:25 -0200

    classe configurando o JAX-RS

---
+++ src/br/com/rcaneppele/rest/config/RestApplication.java
@@ -0,0 +1,9 @@
+package br.com.rcaneppele.rest.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest/")
+public class RestApplication extends Application {
+
+}
