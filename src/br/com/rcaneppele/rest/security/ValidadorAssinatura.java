67ea06f475455925ee6dc9fd0907748c01832af9
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-01-31 12:05:42 -0200

    Alterando injecao para o Construtor

--- src/br/com/rcaneppele/rest/security/ValidadorAssinatura.java
+++ src/br/com/rcaneppele/rest/security/ValidadorAssinatura.java
@@ -8,11 +8,14 @@ import br.com.rcaneppele.rest.resource.Usuario;
 
 public class ValidadorAssinatura {
 
+	private final Usuarios usuarios;
+	private final GeradorDeAssinatura geradorDeAssinatura;
+
 	@Inject
-	private Usuarios usuarios;
-	
-	@Inject
-	private GeradorDeAssinatura geradorDeAssinatura;
+	public ValidadorAssinatura(Usuarios usuarios, GeradorDeAssinatura geradorDeAssinatura) {
+		this.usuarios = usuarios;
+		this.geradorDeAssinatura = geradorDeAssinatura;
+	}
 
 	public boolean isValida(HttpServletRequest request) {
 		Usuario cliente = usuarios.comLogin(request.getHeader("login"));
